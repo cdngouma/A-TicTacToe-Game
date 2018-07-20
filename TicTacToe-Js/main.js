5cee1f9f0df1bcffc555ad2c3b4bafa6679c4f4b
Chrys Ngouma <ngouma.chrys@gmail.com>
2018-07-20 02:12:36 -0400

    implemented AI moves

--- TicTacToe-Js/main.js
+++ TicTacToe-Js/main.js
@@ -2,6 +2,8 @@
 /*	A Tic Tac Toe game
 /*	2018
 **/
+
+// TODO: replace alert messages by better
 var ROW_SIZE = 3,
 	board = [],
 	turn = '',
@@ -13,10 +15,9 @@ var ROW_SIZE = 3,
 	};
 	moves = 0;
 	map = '';			// string representation of board
-	gameMode = 0;		// 0: P vs P 	1: P vs CP
+	gameMode = 1;		// 0: P vs P 	1: P vs CP
 
 function init(){
-	console.log("initializing..");
 	for(let r=0; r<ROW_SIZE; r++){
 		var row = document.createElement('div');
 		$(row).addClass('row');
@@ -34,7 +35,6 @@ function init(){
 		}
 	}
 	updateScores();
-	console.log("...completed");
 	newGame();
 }
 
@@ -55,22 +55,35 @@ function clearBoard(){
 }
 
 function newGame(){
-	console.log('starting new game...');
 	map = '         ';		// 9 spaces
 	moves = 0;
 	round++;
 	turn = round % 2 == 0 ? 'x' : 'o';
-	listenToMoves();
-}
-
-function listenToMoves(){
-	if(gameMode == 1 && round % 2 == 0){
-		aiMoves();
+	// CP start
+	if(gameMode == 1 && round%2==0){
+		setTimeout(function() {aiMoves();}, 400);
 	}
 }
 
 function aiMoves(){
-	// TODO:implements Ai
+	if(turn === 'x'){
+		// AI tries to win
+		let loc = 0;
+		loc = getAILocation('cross');
+		// AI prevent player to win
+		if(loc === 0)
+			loc = getAILocation('round');
+		// AI make random move
+		while(loc === 0){
+			loc = Math.floor(Math.random()*9)+1;
+			loc = checkLocation(loc);
+		}
+		setCell(turn, 'cross', loc);
+		turn = 'o';
+
+		if(checkWins())
+			setWinner();
+	}
 }
 
 function setCell(t, tile, loc){
@@ -92,13 +105,7 @@ function replaceCharAt(str, i, ch){
 }
 
 function checkWins(){
-	if(moves >= 9){
-		alert(gameMode == 0 ? "It\'s a draw!" : 'Not loser but not a winner either');
-		score['tie']++;
-		updateScores();
-		clearBoard();
-	}
-	else if(moves >=5){		// one player made at least 3 moves
+	if(moves >=5 && moves <= ROW_SIZE*ROW_SIZE){		// one player made at least 3 moves
 		let reg_x = RegExp('^xxx|^[ox ]{3}xxx[ox ]{3}$|xxx$|x[ox ]{2}x[ox ]{2}x|x[ox ]{3}x[ox ]{3}x|^[ox ]{2}x[ox ]x[ox ]x', 'g');
 		let reg_o = RegExp('^ooo|^[ox ]{3}ooo[ox ]{3}$|ooo$|o[ox ]{2}o[ox ]{2}o|o[ox ]{3}o[ox ]{3}o|^[ox ]{2}o[ox ]o[ox ]o', 'g');
 
@@ -112,19 +119,103 @@ function checkWins(){
 			score['player2']++;
 			updateScores();
 			clearBoard();
+		}else if(moves >= ROW_SIZE*ROW_SIZE){
+			alert(gameMode == 0 ? "It\'s a draw!" : 'Not a loser but not a winner either');
+			score['tie']++;
+			updateScores();
+			clearBoard();
 		}
 	}
+}
+
+function compareCells(stat, i, tile){
+	if($(board[i]).attr('tile') === tile)
+		return -1;
+
+	if($(board[i]).attr('tile') == undefined)
+		stat.emp++;
+
+	else
+		stat.opp++;
+
+	if(stat.emp > 1 || stat.opp > 0)
+		return 0;
+	
+	return i+1;
+}
+
+function getAILocation(tile){
+	var loc = -1;
+	var stat = {opp: 0, emp: 0};	// # oponent pieces, # empty spot
+	// rows
+	for(let i=0; i<ROW_SIZE*ROW_SIZE; i += 3){
+		stat.opp = 0, stat.emp = 0;
+		for(let j=0; j<ROW_SIZE; j++){
+			let k = compareCells(stat, i+j, tile);
+			loc = k >= 0 ? k : loc;
+			if(loc === 0)
+				break;
+		}
+		
+		if(loc > 0)
+			return loc;
+		loc = -1;
+	}
+	// columns
+	for(let i=0; i<ROW_SIZE; i++){
+		stat.opp = 0, stat.emp = 0;
+		for(let j=0; j<ROW_SIZE*ROW_SIZE; j += 3){
+			let k = compareCells(stat, i+j, tile);
+			loc = k >= 0 ? k : loc;
+			if(loc === 0)
+				break;
+		}
+		if(loc > 0)
+			return loc;
+		loc = -1;
+	}
+	// diagonals to the bottom
+	stat.opp = 0, stat.emp = 0;
+	for(let i=0; i<ROW_SIZE*ROW_SIZE; i += 4){
+		let k = compareCells(stat, i, tile);
+		loc = k >= 0 ? k : loc;
+		if(loc === 0)
+			break;
+	}
+	if(loc > 0)
+		return loc;
+	loc = -1;
+	// diagonals to the top
+	stat.opp = 0, stat.emp = 0;
+	for(let i=ROW_SIZE*ROW_SIZE-3; i>=2; i -= 2){
+		let k = compareCells(stat, i, tile);
+		loc = k >= 0 ? k : loc;
+		if(loc === 0)
+			break;
+	}
+	return loc >= 0 ? loc : 0;
 }
 
 function checkLocation(loc){
 	return $(board[loc-1]).hasClass('used') ? 0 : loc;
 }
+
+function toggleCpP2(){
+	gameMode = gameMode === 0 ? 1 : 0;
+	document.getElementById('name-p2-cp').innerHTML = gameMode === 0 ? 'Player 2':'CP';
+	score['player1']=0;
+	score['player2']=0;		// p2 or CP
+	updateScores();
+	clearBoard();
+}
 // beginning
 init();
+// switch game mode (Default P1 vs CP)
+$('#p2-cp-stats').click(toggleCpP2);
 // player inputs
 $('.cell').click(function(){
-	if(gameMode == 1 && turn != 'o'){console.log('not your turn ['+turn+']');}
-	else{
+	// if(gameMode == 1 && turn != 'o'){}
+	if(!(gameMode == 1 && turn != 'o')){
 		var loc = parseInt(($(this).attr('id')).replace('cell-',''));
 		if(checkLocation(loc) > 0){
 			let tile = turn === 'o'?'round':'cross';
@@ -135,8 +226,6 @@ $('.cell').click(function(){
 				setWinner();
 			else if(gameMode == 1)
 				setTimeout(function() {aiMoves()}, 400);
-		}else{
-			console.log("choose empty location");
 		}
 	}
 });
